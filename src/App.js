cb2dfce21ed1349a6ed229b1a9811f685591a315
tektechnologies <info@tektechnologies.com>
2022-07-27 22:59:25 -0500

    add bootstrap to the form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ListGroupItem } from 'react-bootstrap';
+import { ListGroupItem, Form, Button } from 'react-bootstrap';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 
@@ -21,6 +21,11 @@ import ListGroup from 'react-bootstrap/ListGroup';
     event.preventDefault();
     let userName = event.target.userName.value;
     let selected = event.target.selected.value;
+
+    let firstName = event.target.firstName.value;
+    console.log('first name',firstName);
+
+
     // console.log('user name: ',userName, selected);
     //now after build constructor function
     this.setState({
@@ -75,7 +80,7 @@ handleSelect = (event) => {
 
       // return <ListGroupItem key={index}>{number} - {data[index]}</ListGroupItem>
       return <ListGroupItem key={index}>{number} - {this.state.sortedData[index]}</ListGroupItem>
-      
+
     });
 // Lets take user import to allow the user to choose the numbers they want to see 
 // There are some quarks to forms in REACT so lets take a look
@@ -87,25 +92,29 @@ handleSelect = (event) => {
           {numbers}
         </ListGroup>
         {/* react works this way */}
-        <form onSubmit={this.handleSubmit}>
-          <label>
-          {/* with no back slash we get error */}
-            <input type="text" name="userName" onInput={this.handleInput} />
-          </label>
-          {/* also add a drop down for users to select */}
-          <fieldset>
-            <legend>Selected Numbers</legend>
-            <select name="selected" onChange={this.handleSelect}>
-              <option value="all">All</option>
-              <option value="even">Even</option>
-              <option value="odd">Odd</option>
-            </select>
-          </fieldset>
-          {/* how do we submit a form, there are several ways this is similar to 201 les add a button, add the type for react be explicit for reacts sake
-          So now add event listener just like on click
-           */}
-          <button type="submit">Submit</button>
-        </form>
+        <Form onSubmit={this.handleSubmit}>
+            <Form.Label>Username:
+              <Form.Control type="text" name="userName" onInput={this.handleInput}/>
+            </Form.Label>
+
+            <Form.Group controlId='firstName'>
+              <Form.Label>First Name:</Form.Label>
+              <Form.Control type="text"/>
+            </Form.Group>
+
+            <Form.Label htmlFor="lastName">Last Name:</Form.Label>
+            <Form.Control type="text" name="lastname" id="lastName"/>
+
+            <Form.Group>
+              <p>Selected Numbers</p>
+              <Form.Select name="selected" onChange={this.handleSelect}>
+                <option value="all">All</option>
+                <option value="even">Even</option>
+                <option value="odd">Odd</option>
+              </Form.Select>
+            </Form.Group>
+           <Button type="submit">Submit</Button>
+        </Form>
       </main>
       </>
     )
